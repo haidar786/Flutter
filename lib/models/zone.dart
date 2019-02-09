5b90ab55cab8fca3c320f12ff49881de8fd83a93
TaylorFrazier <github@emrals.com>
2019-02-09 05:27:02 -0500

    Create zone.dart

---
+++ lib/models/zone.dart
@@ -0,0 +1,49 @@
+class Zone {
+  final int id;
+  final String city;
+  final String country;
+  final String slug;
+  final double latitude;
+  final double longitude;
+  final int views;
+  final String image;
+  final String emralsAddress;
+  final String emralsAmount;
+  final String flag;
+  final int subscriberCount;
+  final int cleanupCount;
+
+  Zone({
+    this.id,
+    this.city,
+    this.country,
+    this.slug,
+    this.latitude,
+    this.longitude,
+    this.views,
+    this.image,
+    this.emralsAddress,
+    this.emralsAmount,
+    this.flag,
+    this.subscriberCount,
+    this.cleanupCount,
+  });
+
+  factory Zone.fromJson(Map<String, dynamic> json) {
+    return Zone(
+      id: json['id'] as int,
+      city: json['city'] as String,
+      country: json['country'] as String,
+      slug: json['slug'] as String,
+      latitude: json['latitude'] as double,
+      longitude: json['longitude'] as double,
+      views: json['views'] as int,
+      image: json['image'] as String,
+      emralsAddress: json['emrals_address'] as String,
+      emralsAmount: json['emrals_amount'] as String,
+      flag: json['flag'] as String,
+      subscriberCount: json['subscriber_count'] as int,
+      cleanupCount: json['cleanup_count'] as int,
+    );
+  }
+}

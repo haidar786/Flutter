8ed1b9fbcafa2146bf4177d6f189b453d3dc168a
TaylorFrazier <github@emrals.com>
2019-02-09 01:51:45 -0500

    adding send and receive to wallet

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:emrals/data/database_helper.dart';
 import 'package:emrals/models/user.dart';
+import 'package:flutter/services.dart';
 
 class Settingg extends StatefulWidget {
   const Settingg({Key key}) : super(key: key);
@@ -11,6 +12,8 @@ class Settingg extends StatefulWidget {
 
 class _SettingsPage extends State<Settingg> {
   String userPicture = "https://www.emrals.com/static/images/emrals128.png";
+  String emralsAddress = "";
+  final key = new GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
@@ -26,112 +29,162 @@ class _SettingsPage extends State<Settingg> {
     if (!mounted) return;
     setState(() {
       userPicture = userObject.picture;
+      emralsAddress = userObject.emrals_address;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        key: key,
         appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.account_circle),
+                text: 'profile',
+              ),
+              Tab(
+                icon: Icon(Icons.archive),
+                text: 'receive',
+              ),
+              Tab(
+                icon: RotatedBox(
+                  quarterTurns: 2,
+                  child: Icon(Icons.archive),
+                ),
+                text: 'send',
+              ),
+            ],
+          ),
           title: Text('Settings'),
         ),
-        body: Stack(
-          children: <Widget>[
-            ClipPath(
-              child: Container(color: Colors.black38),
+        body: TabBarView(
+          children: [
+            Stack(
+              children: <Widget>[
+                ClipPath(
+                  child: Container(color: Colors.black38),
+                ),
+                Positioned(
+                    width: 350.0,
+                    top: MediaQuery.of(context).size.height / 9,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                            width: 150.0,
+                            height: 150.0,
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                image: DecorationImage(
+                                    image: NetworkImage(userPicture),
+                                    fit: BoxFit.cover),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(75.0)),
+                                boxShadow: [
+                                  BoxShadow(
+                                      blurRadius: 7.0, color: Colors.black)
+                                ])),
+                        SizedBox(height: 20.0),
+                        Text(
+                          'Tom Cruise',
+                          style: TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromRGBO(33, 219, 244, 1),
+                              fontFamily: 'Montserrat'),
+                        ),
+                        SizedBox(height: 12.0),
+                        Text(
+                          'wallet address',
+                          style: TextStyle(
+                              fontSize: 17.0, fontFamily: 'Montserrat'),
+                        ),
+                        SizedBox(height: 25.0),
+                        Container(
+                            height: 30.0,
+                            width: 120.0,
+                            child: Material(
+                              borderRadius: BorderRadius.circular(20.0),
+                              shadowColor: Colors.black12,
+                              color: Color.fromRGBO(164, 211, 34, 1),
+                              elevation: 7.0,
+                              child: GestureDetector(
+                                onTap: () {},
+                                child: Center(
+                                  child: Text(
+                                    'Edit Name',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'Montserrat'),
+                                  ),
+                                ),
+                              ),
+                            )),
+                        SizedBox(height: 25.0),
+                        Container(
+                            height: 30.0,
+                            width: 120.0,
+                            child: Material(
+                              borderRadius: BorderRadius.circular(20.0),
+                              shadowColor: Colors.black12,
+                              color: Color.fromRGBO(8, 158, 178, 1),
+                              elevation: 7.0,
+                              child: GestureDetector(
+                                onTap: () {
+                                  var db = DatabaseHelper();
+                                  db.deleteUsers().then((_) {
+                                    Navigator.pushNamed(context, '/login');
+                                  });
+                                },
+                                child: Center(
+                                  child: Text(
+                                    'Log out',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: 'Montserrat'),
+                                  ),
+                                ),
+                              ),
+                            ))
+                      ],
+                    ))
+              ],
             ),
-            Positioned(
-                width: 350.0,
-                top: MediaQuery.of(context).size.height / 9,
+            Center(
                 child: Column(
-                  children: <Widget>[
-                    Container(
-                        width: 150.0,
-                        height: 150.0,
-                        decoration: BoxDecoration(
-                            color: Colors.red,
-                            image: DecorationImage(
-                                image: NetworkImage(userPicture),
-                                fit: BoxFit.cover),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(75.0)),
-                            boxShadow: [
-                              BoxShadow(blurRadius: 7.0, color: Colors.black)
-                            ])),
-                    SizedBox(height: 20.0),
-                    Text(
-                      'Tom Cruise',
-                      style: TextStyle(
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromRGBO(33, 219, 244, 1),
-                          fontFamily: 'Montserrat'),
-                    ),
-                    SizedBox(height: 12.0),
-                    Text(
-                      'wallet address',
-                      style: TextStyle(
-                          fontSize: 17.0,
-                          fontFamily: 'Montserrat'),
-                    ),
-                    SizedBox(height: 15.0),
-                    Text(
+              children: <Widget>[
+                SizedBox(height: 15.0),
+                new GestureDetector(
+                  child: Text(
+                    'hdsuhvuehvpsuvbwpibviowbiwort',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                        fontFamily: 'Montserrat'),
+                  ),
+                  onTap: () {
+                    Clipboard.setData(
+                      new ClipboardData(text: 'hdsuhvuehvpsuvbwpibviowbiwort'),
+                    );
+                    key.currentState.showSnackBar(new SnackBar(
+                      content: new Text("Copied to Clipboard"),
+                    ));
+                  },
+                ),
+                SizedBox(height: 15.0),
+                Image.network(
+                  'https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=' +
                       'hdsuhvuehvpsuvbwpibviowbiwort',
-                      style: TextStyle(
-                          fontSize: 11.0,
-                          color: Color.fromRGBO(250, 250, 250, 1),
-                          fontFamily: 'Montserrat'),
-                    ),
-                    SizedBox(height: 25.0),
-                    Container(
-                        height: 30.0,
-                        width: 120.0,
-                        child: Material(
-                          borderRadius: BorderRadius.circular(20.0),
-                          shadowColor: Colors.black12,
-                          color: Color.fromRGBO(164, 211, 34, 1),
-                          elevation: 7.0,
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: Center(
-                              child: Text(
-                                'Edit Name',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Montserrat'),
-                              ),
-                            ),
-                          ),
-                        )),
-                    SizedBox(height: 25.0),
-                    Container(
-                        height: 30.0,
-                        width: 120.0,
-                        child: Material(
-                          borderRadius: BorderRadius.circular(20.0),
-                          shadowColor: Colors.black12,
-                          color: Color.fromRGBO(8, 158, 178, 1),
-                          elevation: 7.0,
-                          child: GestureDetector(
-                            onTap: () {
-                              var db = DatabaseHelper();
-                              db.deleteUsers().then((_) {
-                                Navigator.pushNamed(context, '/login');
-                              });
-                            },
-                            child: Center(
-                              child: Text(
-                                'Log out',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Montserrat'),
-                              ),
-                            ),
-                          ),
-                        ))
-                  ],
-                ))
+                )
+              ],
+            )),
+            Icon(Icons.directions_bike),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }

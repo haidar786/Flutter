a688dc5dcd37f9d6144bc3cd0a713c0fc94aabd2
TaylorFrazier <github@emrals.com>
2019-02-10 13:58:37 -0500

    update emrals amount

--- lib/screens/report_list.dart
+++ lib/screens/report_list.dart
@@ -13,35 +13,6 @@ class ReportListWidget extends StatefulWidget {
   _ReportList createState() => _ReportList();
 }
 
-class DialogDemoItem extends StatelessWidget {
-  const DialogDemoItem(
-      {Key key, this.icon, this.color, this.text, this.onPressed})
-      : super(key: key);
-
-  final IconData icon;
-  final Color color;
-  final String text;
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return SimpleDialogOption(
-      onPressed: onPressed,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Icon(icon, size: 36.0, color: color),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0),
-            child: Text(text),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class _ReportList extends State<ReportListWidget> {
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
       new GlobalKey<RefreshIndicatorState>();
@@ -294,7 +265,7 @@ class _ReportList extends State<ReportListWidget> {
                                 showDialog(
                                     context: context,
                                     builder: (ctx) {
-                                      return TipDialog(reports[index].id);
+                                      return TipDialog(reports[index]);
                                     });
                               },
                               child: Text("TIP"),

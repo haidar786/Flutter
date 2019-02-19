f529850b24ea419e9eed1c84b259cdb3cb9edd6e
TaylorFrazier <github@emrals.com>
2019-02-19 00:26:32 -0500

    bump version

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ sed -i -e "s/GOOGLE_MAPS_API_KEY/${GOOGLE_MAPS_API_KEY}/g" android/app/src/main/
 sed -i -e "s/GOOGLE_MAPS_API_KEY/${GOOGLE_MAPS_API_KEY}/g" ios/Runner/AppDelegate.m
 sed -i -e "s/GOOGLE_MAPS_API_KEY/${GOOGLE_MAPS_API_KEY}/g" ios/Runner/AppDelegate.swift
 sed -i -e "s/BUILD_NUMBER/${BITRISE_BUILD_NUMBER}/g" lib/screens/login_screen.dart
-sed -i -e "s/APP_VERSION_NUMBER/0.2.0/g" lib/screens/login_screen.dart
+sed -i -e "s/APP_VERSION_NUMBER/0.2.2/g" lib/screens/login_screen.dart
 sed -i -e "s/BUILD_NUMBER/${BITRISE_BUILD_NUMBER}/g" lib/screens/settings.dart
-sed -i -e "s/APP_VERSION_NUMBER/0.2.0/g" lib/screens/settings.dart
+sed -i -e "s/APP_VERSION_NUMBER/0.2.2/g" lib/screens/settings.dart
 sed -i -e "s~SENTRY_DSN~${SENTRY_DSN}~g" lib/main.dart
 

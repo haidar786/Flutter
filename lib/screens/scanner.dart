363f6aa6cb61fdeb717e9ad62c0bcf48fa31bc30
TaylorFrazier <github@emrals.com>
2019-02-24 03:14:01 -0500

    show snackbar on scanner

--- lib/screens/scanner.dart
+++ lib/screens/scanner.dart
@@ -4,13 +4,17 @@ import 'package:qr_mobile_vision/qr_camera.dart';
 class Scanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 300.0,
-      height: 600.0,
-      child: new QrCamera(
-        qrCodeCallback: (code) {
-          print(code);
-        },
+    return Center(
+      child: SizedBox(
+        width: 350.0,
+        height: 350.0,
+        child: new QrCamera(
+          qrCodeCallback: (code) {
+            print(code);
+            final snackBar = SnackBar(content: Text(code));
+            Scaffold.of(context).showSnackBar(snackBar);
+          },
+        ),
       ),
     );
   }

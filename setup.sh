077150948032c6727f0dfe5d5d57804534406104
TaylorFrazier <github@emrals.com>
2019-03-15 00:47:30 -0400

    prepare for release

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
-sed -i -e "s/GOOGLE_MAPS_API_KEY/${GOOGLE_MAPS_API_KEY}/g" android/app/src/main/AndroidManifest.xml
-sed -i -e "s/GOOGLE_MAPS_API_KEY/${GOOGLE_MAPS_API_KEY}/g" ios/Runner/AppDelegate.m
-sed -i -e "s/GOOGLE_MAPS_API_KEY/${GOOGLE_MAPS_API_KEY}/g" ios/Runner/AppDelegate.swift
-sed -i -e "s/BUILD_NUMBER/${BITRISE_BUILD_NUMBER}/g" lib/screens/login_screen.dart
-sed -i -e "s/APP_VERSION_NUMBER/0.2.4/g" lib/screens/login_screen.dart
-sed -i -e "s/BUILD_NUMBER/${BITRISE_BUILD_NUMBER}/g" lib/screens/settings.dart
-sed -i -e "s/APP_VERSION_NUMBER/0.2.4/g" lib/screens/settings.dart
+sed -i -e "s/GOOGLE_MAPS_API_KEY/${GOOGLE_MAPS_API_KEY}/g" android/app/src/main/AndroidManifest.xml ios/Runner/AppDelegate.m ios/Runner/AppDelegate.swift
+sed -i -e "s/BUILD_NUMBER/${BITRISE_BUILD_NUMBER}/g" lib/screens/login_screen.dart lib/screens/settings.dart lib/main.dart
+sed -i -e "s/APP_VERSION_NUMBER/0.2.5/g" lib/screens/login_screen.dart lib/screens/settings.dart lib/main.dart
 sed -i -e "s~SENTRY_DSN~${SENTRY_DSN}~g" lib/main.dart
-sed -i -e "s/APP_VERSION_NUMBER/0.2.4/g" lib/main.dart
-sed -i -e "s/BUILD_NUMBER/${BITRISE_BUILD_NUMBER}/g" lib/main.dart
 
